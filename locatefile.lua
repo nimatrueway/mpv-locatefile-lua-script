1a87d47d4a1ec5b0a4bc762c316d0e8ca324cf53
Nima Taheri <nima.trueway@gmail.com>
2018-01-18 11:32:47 +0330

    Update locatefile.lua

--- locatefile.lua
+++ locatefile.lua
@@ -7,16 +7,49 @@ function GetDirectory(url)
 end
 
 -- for ubuntu
-file_browser_cmd = "nautilus '$path'"
+file_browser_linux_cmd = "nautilus '$path'"
 -- for macos
--- file_browser_cmd = "open -a Finder '$path'"
+file_browser_macos_cmd = "open -a Finder '$path'"
 -- for windows
--- file_browser_cmd = "explorer /select,\"$path\""
+file_browser_windows_cmd = "explorer /select,\"$path\""
+
+--// check if macos
+function is_macos()
+  local homedir = os.getenv("HOME")
+  if homedir ~= nil and homedir.sub(1, 6) ~= "/Users" then
+    return true
+  else
+    return false
+  end
+end
+
+--// check if windows
+function is_windows()
+  local windir = os.getenv("windir")
+  if windir~=nil then
+    return true
+  else
+    return false
+  end
+end
 
 --// handle "locate-current-file" function triggered by a key in "input.conf"
 mp.register_script_message("locate-current-file", function()
+	file_browser_cmd = file_browser_linux_cmd
 	local filepath = mp.get_property("path")
+
+	if is_windows() then
+		file_browser_cmd = file_browser_windows_cmd
+		filepath = filepath:gsub("/", "\\")
+	elseif is_macos() then
+		file_browser_cmd = file_browser_macos_cmd
+	else
+		file_browser_cmd = file_browser_linux_cmd
+	end	
+
 	if filepath ~= nil then
-		os.execute(file_browser_cmd:gsub("$path", filepath))
+		cmd = file_browser_cmd:gsub("$path", filepath)
+		mp.osd_message(cmd)
+		os.execute(cmd)
 	end
 end)

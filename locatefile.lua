c0f83535b5fa6111c6ea94eb3d0e588846babbd6
Nima Taheri <nima.trueway@gmail.com>
2018-03-04 22:29:55 +0330

    Update locatefile.lua

--- locatefile.lua
+++ locatefile.lua
@@ -9,14 +9,15 @@ end
 -- for ubuntu
 file_browser_linux_cmd = "nohup nautilus --select \"$path\" &"
 -- for macos
-file_browser_macos_cmd = "open -a Finder \"$path\""
+file_browser_macos_cmd_osascript_content = 'tell application "Finder"' .. '\n' .. 'set frontmost to true' .. '\n' .. 'reveal (POSIX file "$path")' .. '\n' .. 'end tell' .. '\n'
+file_browser_macos_cmd = 'osascript "$osascript_file"'
 -- for windows
 file_browser_windows_cmd = "explorer /select,\"$path\""
 
 --// check if macos
 function is_macos()
   local homedir = os.getenv("HOME")
-  if homedir ~= nil and homedir.sub(1, 6) == "/Users" then
+  if homedir ~= nil and string.sub(homedir,1,6) == "/Users" then
     return true
   else
     return false
@@ -33,6 +34,15 @@ function is_windows()
   end
 end
 
+--// create temporary script
+function create_temp_file(content)
+  local tmp_filename = os.tmpname()
+  local tmp_file = io.open(tmp_filename, "wb")
+  tmp_file:write(content)
+  io.close(tmp_file)
+  return tmp_filename
+end
+
 --// handle "locate-current-file" function triggered by a key in "input.conf"
 mp.register_script_message("locate-current-file", function()
   file_browser_cmd = file_browser_linux_cmd
@@ -41,13 +51,14 @@ mp.register_script_message("locate-current-file", function()
     file_browser_cmd = file_browser_windows_cmd
     filepath = filepath:gsub("/", "\\")
   elseif is_macos() then
-    file_browser_cmd = file_browser_macos_cmd
+    local content = file_browser_macos_cmd_osascript_content:gsub("$path", filepath)    
+    file_browser_cmd = file_browser_macos_cmd:gsub("$osascript_file", create_temp_file(content))
   else
     file_browser_cmd = file_browser_linux_cmd
   end	
   if filepath ~= nil then
     cmd = file_browser_cmd:gsub("$path", filepath)
-    mp.osd_message('Browse ' .. filepath)
+    mp.osd_message('Browse \n' .. filepath)
     os.execute(cmd)
   end
 end)
